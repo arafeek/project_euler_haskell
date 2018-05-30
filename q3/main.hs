dbfb40f91b2af168bedcccbf3a13d77ee355a438
arafeek <adam_rafeek@hotmail.com>
2018-05-29 22:36:40 -0400

    Better solution to q3

--- q3/main.hs
+++ q3/main.hs
@@ -4,16 +4,27 @@ The prime factors of 13195 are 5, 7, 13 and 29.
 What is the largest prime factor of the number 600851475143 ?
 -}
 
--- The following is a very naive solution with a pretty bad runtime
--- I will need to improve this, but it provides a good starting point for a solution
-factors :: Int -> [Int]
-factors n = [x | x <- [1..n], n `mod` x == 0]
+import System.Environment
 
-isPrime :: Int -> Bool
+-- Returns the rounded square root of any integer
+intRoot ::  Int  -> Int
+intRoot n = round $ sqrt $ fromIntegral n
+
+-- Returns a list containing all factors of a number
+factors :: Int -> [Int]
+factors n = [x | x <- [1..intRoot n], n `mod` x == 0] ++ [n]
+
+-- Returns True if n is prime, false otherwise
+isPrime :: Int-> Bool
 isPrime n = factors n == [1, n]
 
+-- Returns the largest prime factor of an Int n
 largestPrimeFactor :: Int -> Int
-largestPrimeFactor n = maximum (filter isPrime (factors n))
+largestPrimeFactor n = maximum $ filter isPrime $ factors n
 
 main :: IO ()
-main = print $ largestPrimeFactor 100
+main = do
+  args <- getArgs
+  let arg = head args
+  let n = (read arg) :: Int
+  print $ largestPrimeFactor n
